344587b64f243b6c49be75dbde4dede91fef8564
magda-gierg <mjgiergiczny@gmail.com>
2018-06-21 12:26:22 +1200

    switchNameHandler added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import Person from './Person'
 import './App.css';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Max", age: 24},
+      {name: "Julia", age: 4},
+      {name: "Tom", age: 40}
+    ]
+  }
+switchNameHandler = () => {
+console.log("was clicked!")
+}
+
   render() {
     return (
       <div className="App">
-      <h1>Hi</h1>
-      <Person name="Max" age="24"/>
-      <Person name="Julia" age="4">I like ice-cream.</Person>
-      <Person name="Tom" age="40"/>
+      <h1>Hi, I am react app!</h1>
+      <button onClick={this.switchNameHandler} >Switch name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I like ice-cream.</Person>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
