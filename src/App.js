951aee136ac035ed53a95aa248c91eecc9b76a30
magda-gierg <mjgiergiczny@gmail.com>
2018-06-29 10:50:03 +1200

    flexible lists

--- src/App.js
+++ src/App.js
@@ -5,59 +5,62 @@ import './App.css';
 class App extends Component {
   state = {
     persons: [
-      {name: "Max", age: 24},
-      {name: "Julia", age: 4},
-      {name: "Tom", age: 40}
+      {id: "acs", name: "Max", age: 24},
+      {id: "jik", name: "Julia", age: 4},
+      {id: "dgs", name: "Tom", age: 40}
     ],
     showPersons: false
   }
 
-changeNameHandler = (event) => {
-  this.setState({
-    persons: [
-      {name: "Max", age: 24},
-      {name: "Julia", age: 4},
-      {name: event.target.value, age: 40}
-    ]
-  })
-}
+  changeNameHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id
+    })
+    const person = {...this.state.persons[personIndex]}
+    person.name = event.target.value
+    const persons = [...this.state.persons]
+    persons[personIndex] = person
+    this.setState({ persons: persons})
+  }
 
-togglePersonsHandler = () => {
-  const doesShow = this.state.showPersons
-  this.setState({showPersons: !doesShow })
-}
 
-deletePersonHandler = (personIndex) => {
-  // const persons = this.state.persons.slice()
-  const persons = [...this.state.persons]
-  persons.splice(personIndex, 1)
-  this.setState({persons: persons})
-}
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons
+    this.setState({showPersons: !doesShow })
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice()
+    const persons = [...this.state.persons]
+    persons.splice(personIndex, 1)
+    this.setState( {persons: persons} )
+  }
 
   render() {
     let persons = null
-      if (this.state.showPersons) {
-        persons = (
-          <div>{this.state.persons.map((person, index) => {
-            return <Person
-            name={person.name}
-            age={person.age}
-            click={() => this.deletePersonHandler(index)} />
-            {/*click={this.deletePersonHandler.bind(this, index)}*/}
-            })
-          }
-        </div>
-      )
-}
-
-    return (
-      <div className="App">
-        <h1>Hi, I am React App!</h1>
-        <button onClick={this.togglePersonsHandler} >Toggle Persons</button>
-        {persons}
+    if (this.state.showPersons) {
+      persons = (
+        <div>{this.state.persons.map((person, index) => {
+          return <Person
+          name={person.name}
+          age={person.age}
+          click={() => this.deletePersonHandler(index)}
+          key={person.id}
+          changed={(event) => this.changeNameHandler(event, person.id)} />
+        })
+      }
       </div>
-    );
+    )
   }
+
+  return (
+    <div className="App">
+    <h1>Hi, I am React App!</h1>
+    <button onClick={this.togglePersonsHandler} >Toggle Persons</button>
+    {persons}
+    </div>
+  );
+}
 }
 
 export default App;
